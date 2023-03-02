3db8259e78113ef6d342b2e3ba9f178bde4ec8c0
julian0072 <eslon_44@hotmail.com>
2023-03-02 10:24:11 -0300

    Cambio de nombre de Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import ComponenteNavbar from "./components/ComponenteNavbar";
+import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
     <>
-      <ComponenteNavbar />
+      <NavBar />
       <ItemListContainer />
     </>
   );
