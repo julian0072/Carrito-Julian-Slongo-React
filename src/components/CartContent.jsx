6e6e212a38ad9855f08136420c87805ea90a521e
julian0072 <eslon_44@hotmail.com>
2023-03-17 21:35:08 -0300

    cambios carrito

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -7,23 +7,24 @@ function BasicExample() {
   return (
     <>
       <h2 className="display-5 fw-bold text-light py-5">Carrito</h2>
-      <div className="container-fluid p-5 ">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+      <div className="container-fluid p-5 w-50 ">
+        <Form className="pb-5">
+          <Form.Group
+            className="mb-3 text-light fw-bold"
+            controlId="formBasicEmail"
+          >
+            <Form.Label className="py-5 display-5">Test</Form.Label>
+            <Form.Control type="name" placeholder="Name" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group
+            className="mb-3 text-light fw-bold"
+            controlId="formBasicPassword"
+          >
+            <Form.Label className="py-5 display-5">Test</Form.Label>
+            <Form.Control type="name" placeholder="Name" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
